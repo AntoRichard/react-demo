a3155cbd63fc412bdfc953abe7e071492ba7bf1c
cybersrikanth <41125331+cybersrikanth@users.noreply.github.com>
2020-07-11 12:33:24 +0530

    Update HttpClient.js

--- src/service/HttpClient.js
+++ src/service/HttpClient.js
@@ -12,18 +12,6 @@ export default function HttpClient(name) {
       });
       resolve(dataSet)
     })
-    let temp = {}
-    let dates = []
-    for (let i = 1; i < 1000; i++) {
-      var current = new Date()
-      var date = new Date(current.getTime() + ((i + 1) * 1440000));
-      dates.push([date, 100000 + (i * i * i)])
-    }
-    temp.data = dates
-    temp.orgName = name
-
-
-    return temp
   })
 
 }
